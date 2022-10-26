b63a05ae5e7b8b0c039e1bd9c0acfca0f217b036
Sandeep <sndpkrl007@gmail.com>
2022-10-26 00:09:51 -0500

    New Changes

--- src/ui/component/Body/Body.js
+++ src/ui/component/Body/Body.js
@@ -10,23 +10,28 @@ import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { useState } from "react";
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom"; 
 
 
 
 const Body = () => {
+  const navigate = useNavigate();
+
   const [openDate, setOpenDate] = useState(false);
-  const [dates, setDates] = useState([
-    {
+  const [dates, setDates] = useState({
       startDate: new Date(),
       endDate: new Date(),
       key: "selection",
-    },
-  ]);
+    });
+
+  const searchButtonClicked = () => {
+    navigate('/view', {state: dates});
+  };
 
   return (
     <div className="rentalbody">
           <div className="search">
-                <form action="" className="searchbar">
+                <div className="searchbar">
                     <input type={"text"} placeholder="Search for vechicles" name="vec"/>
 
                     <div className="datesearch">
@@ -36,27 +41,25 @@ const Body = () => {
                                     onClick={() => setOpenDate(!openDate)}
                                     className="dateSearchText"
                                   >
-                                  {`${format(dates[0].startDate, "MM/dd/yyyy")} To ${format(
-                                    dates[0].endDate,
+                                  {`${format(dates.startDate, "MM/dd/yyyy")} To ${format(
+                                    dates.endDate,
                                     "MM/dd/yyyy"
                                   )}`}
                                   </span>
                                   {openDate && (
                                     <DateRange
                                       editableDateInputs={true}
-                                      onChange={(item) => setDates([item.selection])}
+                                      onChange={(item) => setDates(item.selection)}
                                       moveRangeOnFirstSelection={false}
-                                      ranges={dates}
+                                      ranges={[dates]}
                                       className="date"
                                       minDate={new Date()}
                                     />
                                   )}
                         </div>
                     </div>  
-
-                    <button type="submit"> <img src={search} alt="search"/> </button>
-                </form> 
-      
+                    <button onClick={searchButtonClicked}> <img src={search} alt="search"/> </button>
+                </div> 
           </div>
     </div>
   )
--- src/ui/component/view/View.js
+++ src/ui/component/view/View.js
@@ -1,8 +1,12 @@
+import { useLocation } from "react-router-dom"
 import Navbars from "../Navbars/Navbars"
 import VBody from "./Vbody/VBody"
 
 
 const View = () => {
+  const location = useLocation();
+  console.log(location);
+  
   return (
     <>
         <Navbars/>
