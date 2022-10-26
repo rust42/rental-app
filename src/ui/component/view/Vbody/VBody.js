db50736de1fa69bcefd8b461b467129854525e49
Sandeep <sndpkrl007@gmail.com>
2022-10-26 01:25:40 -0500

    Format

--- src/ui/component/view/Vbody/VBody.js
+++ src/ui/component/view/Vbody/VBody.js
@@ -1,89 +1,294 @@
-import "../Vbody/VBody.css"
-import { Box, CircularProgress } from "@mui/material";
-
-
-const VBody = ({children}) => {
-  return (
-    <div className="viewbody">
-
-        <div className="filters">
+import "../Vbody/VBody.css";
+const VBody = ({ children }) => {
+    return (
+        <div className="viewbody">
+            <div className="filters">
                 <h2> FILTER BY </h2>
 
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item"> Exclusive Offers </li> </h3> <br/>
-                    <li className="item"> <label> <input type={"checkbox"}/> Deals </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Inquires </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Sign In </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Sign Up </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Help and FAQs </label> </li>
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Exclusive Offers </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Deals{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Inquires{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Sign In{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Sign Up{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Help and FAQs{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
 
-
-
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item"> Vechicle type </li> </h3> <br/>
-                    <li className="item"> <label> <input type={"checkbox"}/> Sedan </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Convertible </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> SUVs </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Minivans </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Pickup Trucks </label> </li>
-
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Vechicle type </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Sedan{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Convertible{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> SUVs{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Minivans{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Pickup Trucks{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
 
-
-
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item"> Car types </li> </h3> <br/>
-                    <li className="item"> <label> <input type={"checkbox"}/> Mini </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Economy </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Compact </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Mid-size </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Standard </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Full-Size </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> SUVs </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Pickup </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Others </label> </li>
-
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Car types </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Mini{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Economy{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Compact{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Mid-size{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Standard{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Full-Size{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> SUVs{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Pickup{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Others{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
 
-
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item">  Capacity </li> </h3> <br/>
-                    <li className="item"> <label> <input type={"checkbox"}/> 2-5 passengers </label> </li>
-
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Capacity </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> 2-5 passengers{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
 
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item">  Insurances </li> </h3> <br/>
-                    <li className="item"> <label> <input type={"checkbox"}/> Driver only </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Full Insurances </label> </li>
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Insurances </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Driver only{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Full Insurances{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
 
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item">  Total Price </li> </h3> <br/>
-
-                    <li className="item"> <label> <input type={"checkbox"}/> Less than $100 </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> $100 - $200 </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> $200 - $300 </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> $300 - $400 </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Greater than $400 </label> </li>
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Total Price </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Less than $100{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> $100 - $200{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> $200 - $300{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> $300 - $400{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Greater than $400{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
 
-                <ul className="filterlist"> <br/>
-                    <h3>  <li className="item">  Specifications </li> </h3> <br/>
-                    <li className="item"> <label> <input type={"checkbox"}/> Automatic </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Manual </label> </li>
-                    <li className="item"> <label> <input type={"checkbox"}/> Unlimited Mileages </label> </li>
+                <ul className="filterlist">
+                    {" "}
+                    <br />
+                    <h3>
+                        {" "}
+                        <li className="item"> Specifications </li>{" "}
+                    </h3>{" "}
+                    <br />
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Automatic{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Manual{" "}
+                        </label>{" "}
+                    </li>
+                    <li className="item">
+                        {" "}
+                        <label>
+                            {" "}
+                            <input type={"checkbox"} /> Unlimited Mileages{" "}
+                        </label>{" "}
+                    </li>
                 </ul>
+            </div>
 
+            <div className="viewcards">{children}</div>
         </div>
+    );
+};
 
-        <div className="viewcards">
-            {children}
-        </div>
-        
-    </div>
-  )
-}
-
-export default VBody
+export default VBody;
--- src/ui/component/view/View.js
+++ src/ui/component/view/View.js
@@ -1,46 +1,51 @@
-import { useLocation } from "react-router-dom"
-import Navbars from "../Navbars/Navbars"
-import VBody from "./Vbody/VBody"
-import VehicleItem from './Vbody/VehicleItem';
+import Navbars from "../Navbars/Navbars";
+import VBody from "./Vbody/VBody";
+import VehicleItem from "./Vbody/VehicleItem";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { fetchVehicles as getVehicles } from '../../../reducers/vehicle'
+import { fetchVehicles as getVehicles } from "../../../reducers/vehicle";
 import { Box, CircularProgress } from "@mui/material";
 
 import SortBar from "./Vbody/SortBar";
 
-
 const View = () => {
-  const location = useLocation();
-  
   const dispatch = useDispatch();
-  const vehicleState = useSelector(selector => selector.vehicles);
-  const { http, vehicleList } = vehicleState
+  const vehicleState = useSelector((selector) => selector.vehicles);
+  const { http, vehicleList } = vehicleState;
 
   useEffect(() => {
-      fetchVehicles()
-  }, [])
+    fetchVehicles();
+  });
 
   const fetchVehicles = async () => {
-      dispatch(getVehicles())
-  }
+    dispatch(getVehicles());
+  };
+
   return (
     <>
-        <Navbars/>
-        <VBody>
-          {http.loading &&
-                    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                    <Box sx={{ display: 'flex' }}>
-                      <CircularProgress />
-                    </Box>
-                </div>}
-          <SortBar />
+      <Navbars />
+      <VBody>
+        {http.loading && (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box sx={{ display: "flex" }}>
+              <CircularProgress />
+            </Box>
+          </div>
+        )}
+        <SortBar />
 
-          {vehicleList.map(vehicle => <VehicleItem key = {vehicle.id} vehicle={vehicle} />)}
-        </VBody>
+        {vehicleList.map((vehicle) => (
+          <VehicleItem key={vehicle.id} vehicle={vehicle} />
+        ))}
+      </VBody>
     </>
-  
-  )
-}
+  );
+};
 
-export default View
+export default View;
